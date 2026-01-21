bd72f16e67b8d60f3b4deb83831153d0600e20b0
PigsRpeople2 <167029562+PigsRpeople2@users.noreply.github.com>
2026-01-21 17:42:31 +1100

    built in emergency button
    currently just looks good an prints to console

--- UI/src/afr/afrapp.tsx
+++ UI/src/afr/afrapp.tsx
@@ -1,8 +1,48 @@
-import { useState } from 'react'
+import { use, useEffect, useRef, useState } from 'react'
 import '../app.css'
 
 function AfrApp() {
   let buttons = [{text: "NAV", function: () => {}}, {text: "MAP", function: () => {}}, {text: "SitR", function: () => {}}]
+
+  const [duress, setDuress] = useState(false)
+  const [duressheld, setDuressheld] = useState(false)
+  const duressTimerRef = useRef(0)
+
+  const handleDuress = () => {
+    console.log("oh shit")
+  }  
+
+
+
+  const handleDuressMouseDown = () => {
+      setDuressheld(true)
+      duressTimerRef.current = setTimeout(() => {
+        setDuressheld(false)
+        if (duress == false) {
+          handleDuress()
+        console.log("Duress Activated")
+        setDuress(true)}
+        else if (duress == true) {
+          console.log("Duress Deactivated")
+          setDuress(false)
+        }
+      }, 1200);
+      
+      
+
+    
+  }
+
+  const handleDuressMouseUp = () => {
+    setDuressheld(false)
+    clearTimeout(duressTimerRef.current)
+  }
+
+  useEffect(() => {
+    clearTimeout(duressTimerRef.current)
+  }, [])
+
+
 
 
 
@@ -17,10 +57,22 @@ function AfrApp() {
             {buttons.map((button, index) => (
               <div key={index} className='afr-bottom-navbar-button' onClick={button.function}>
                 <span className='afr-bottom-navbar-button-container' onClick={button.function}>
-                  <p>{button.text}</p>
+                  <p style={{userSelect: "none"}}>{button.text}</p>
                 </span>
               </div>
             ))}
+            <div className='afr-emergency-button'>
+              {duress ==false ?
+              (<div className='afr-emergency-button-interior' onMouseDown={handleDuressMouseDown} onMouseUp={handleDuressMouseUp} onMouseLeave={handleDuressMouseUp}/> ) 
+              : 
+              (<div className='afr-emergency-button-interior-active' onMouseDown={handleDuressMouseDown} onMouseUp={handleDuressMouseUp} onMouseLeave={handleDuressMouseUp}/> )}
+              {duressheld == true && 
+                <svg viewBox='0 0 75 75' className='afr-emergency-progress-svg'>
+                  <circle className="emergency-progress" cx="45" cy="60" r="35"  stroke='rgba(255,110,1,0.7)' fill='none'>
+                  
+                  </circle>
+                </svg>}
+            </div>
           </div>
         </div>
       </div>
@@ -29,3 +81,5 @@ function AfrApp() {
 }
 
 export default AfrApp
+
+
